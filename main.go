debdbbe67115e82af224d14050ff0bb36873fbdf
FlowRays <1581561246@qq.com>
2023-05-21 17:16:21 +0800

    feat: Add landmark and relevant api

--- main.go
+++ main.go
@@ -28,9 +28,11 @@ type Database struct {
 	DB *gorm.DB
 }
 
-type Image struct {
-	ID   uint   `gorm:"primaryKey"`
-	Path string `gorm:"size:255"`
+type Landmark struct {
+	ID        uint   `gorm:"primaryKey"`
+	Path      string `gorm:"size:255"`
+	Latitude  string `gorm:"not null"`
+	Longitude string `gorm:"not null"`
 }
 
 func main() {
@@ -47,7 +49,7 @@ func main() {
 	}
 
 	// 执行数据库迁移
-	err = db.AutoMigrate(&Image{})
+	err = db.AutoMigrate(&Landmark{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -87,17 +89,61 @@ func connectDB(config Config) (*gorm.DB, error) {
 }
 
 func registerAPI(r *gin.Engine, db *Database) {
+	// 定义处理 CORS 请求的中间件
+	// corsMiddleware := func() gin.HandlerFunc {
+	// 	return func(c *gin.Context) {
+	// 		// 允许的域名列表
+	// 		allowedOrigins := []string{"http://localhost:3000", "http://172.27.144.70:5941"}
+
+	// 		origin := c.Request.Header.Get("Origin")
+	// 		// 检查请求的来源是否在允许的域名列表中
+	// 		for _, allowedOrigin := range allowedOrigins {
+	// 			if allowedOrigin == origin {
+	// 				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+	// 				break
+	// 			}
+	// 		}
+
+	// 		// 设置其他 CORS 相关的响应头
+	// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// 		// 处理预检请求
+	// 		if c.Request.Method == "OPTIONS" {
+	// 			c.AbortWithStatus(200)
+	// 			return
+	// 		}
+
+	// 		// 调用下一个中间件或处理函数
+	// 		c.Next()
+	// 	}
+	// }
+
+	// 应用 CORS 中间件
+	// r.Use(corsMiddleware())
+
 	r.POST("/upload", func(c *gin.Context) {
-		uploadImage(c, db)
+		createLandmark(c, db)
+	})
+
+	r.GET("/get", func(c *gin.Context) {
+		getAllLandmark(c, db)
+	})
+
+	r.GET("/image/:id", func(c *gin.Context) {
+		getImage(c, db)
 	})
 }
 
-func uploadImage(c *gin.Context, db *Database) {
+func createLandmark(c *gin.Context, db *Database) {
 	file, err := c.FormFile("image")
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	latitude := c.PostForm("latitude")
+	longitude := c.PostForm("longitude")
 
 	path := "uploads/" + file.Filename
 
@@ -107,13 +153,45 @@ func uploadImage(c *gin.Context, db *Database) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
-
-	image := Image{Path: path}
-	result := db.DB.Create(&image)
+	landmark := Landmark{Path: path, Latitude: latitude, Longitude: longitude}
+	result := db.DB.Create(&landmark)
 	if result.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "Image uploaded successfully!"})
+}
+
+func getAllLandmark(c *gin.Context, db *Database) {
+	var landmarks []Landmark
+	if err := db.DB.Find(&landmarks).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch landmarks"})
+		return
+	}
+	c.JSON(http.StatusOK, landmarks)
+}
+
+func getImage(c *gin.Context, db *Database) {
+	id := c.Param("id")
+
+	// 根据 ID 查询数据库，获取对应的路径
+	var landmark Landmark
+	if err := db.DB.Where("id = ?", id).First(&landmark).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Image not found"})
+		return
+	}
+
+	// 读取图片文件
+	imageBytes, err := ioutil.ReadFile(landmark.Path)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read image file"})
+		return
+	}
+
+	// 设置响应头为图片类型
+	c.Header("Content-Type", "image/jpeg")
+
+	// 返回图片数据
+	c.Data(http.StatusOK, "image/jpeg", imageBytes)
 }
